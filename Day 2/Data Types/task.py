1099c7ca8d5f0ceacd973dc7ff1cef019f29f0d9
Leo Abrantes <leoabrantesmelo@gmail.com>
2024-11-08 14:32:44 -0300

    day two (part1)

--- Day 2/Data Types/task.py
+++ Day 2/Data Types/task.py
@@ -1,1 +1,21 @@
-len("Hello")
+
+#substring
+print("Hellorstuvw"[0])
+
+#String
+print("123"+"456")
+
+#Integer = Whole number
+print(123+345)
+
+#Large integers
+print(123_000+345_000)
+
+#Float = Floating Point Number
+print(3.14159)
+
+#Boolean
+print(True)
+print(False)
+
+
--- Day 2/Type Error, Checking and Conversion/task.py
+++ Day 2/Type Error, Checking and Conversion/task.py
@@ -1,1 +1,18 @@
-len(12345)
+len("12345")
+
+
+print(type(555))
+print(type(555.55))
+print(type("555.55"))
+print(type(True))
+
+print(int("123")+int("456"))
+
+
+name_of_the_user = input("Enter your name")
+length_of_name = len(name_of_the_user)
+
+print(type("Number of letters in your name: "))  # str
+print(type(length_of_name))  # int
+
+print("Number of letters in your name: " + str(length_of_name))
